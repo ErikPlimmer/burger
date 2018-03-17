b95c6c565a25daef42bc9b616670f9624b6cddae
Erik Plimmer <Elp013@Gmail.com>
2018-03-17 08:56:52 -0400

    change all cats to burgers

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheese');
+INSERT INTO burgers (burger_name) VALUES ('Mushroom');
+INSERT INTO burgers (burger_name) VALUES ('Buffalo');
+INSERT INTO burgers (burger_name) VALUES ('Tofu');
